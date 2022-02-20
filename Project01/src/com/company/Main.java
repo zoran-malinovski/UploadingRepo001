97eb68258ce23e719c126e748a023a7c50d2baaf
zoran-malinovski <46956073+zoran-malinovski@users.noreply.github.com>
2022-02-20 20:12:42 +0100

    Add files via upload
    Uploading file - first commit

---
+++ Project01/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+// TODO: to add the rest of the elements
+// Remember to put comment
